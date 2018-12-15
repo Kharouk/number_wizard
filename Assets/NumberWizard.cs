79c58889fa35d6a13b293eb2651dd26c52c5b690
Alex <akharoukpp@gmail.com>
2018-12-15 18:39:53 +0000

    ADD user input and display result.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,14 +7,31 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log($"Welcome to the Magical Number Wizard! Ready to play?");
-        Debug.Log($"Please pick a number between 1 and 1000");
+        // Setting our variables:
+        int max = 1000;
+        int min = 1;
+        string title = "Magical Number Wizard";
 
+        Debug.Log($"Welcome to the {title}!");
+        Debug.Log($"Please pick a number between {min} and {max}");
+        Debug.Log($"Is your number higher or lower than {max/2}?");
+        Debug.Log("Push Up = higher, Push Down = lower, Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up arrow was pressed.");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down arrow was pressed.");
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter was pressed.");
+        }
     }
 }
