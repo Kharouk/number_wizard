57e44de61ca04dbb25abb86dfeca432f2b9521dd
Alex <akharoukpp@gmail.com>
2018-12-15 18:15:32 +0000

    ADD C# file and welcome player.

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"Welcome to the Magical Number Wizard! Ready to play?");
+        Debug.Log($"Please pick a number between 1 and 1000");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
