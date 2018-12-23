de557c3b07739e85a0815e9942fa078a2a4aca05
Alex <akharoukpp@gmail.com>
2018-12-23 14:53:29 +0000

    ADD formula to calculate guess.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -16,8 +16,10 @@ public class NumberWizard : MonoBehaviour
 
         Debug.Log($"Welcome to the {title}!");
         Debug.Log($"Please pick a number between {min} and {max}");
-        Debug.Log($"Is your number higher or lower than {max/2}?");
-        Debug.Log("Push Up = higher, Push Down = lower, Enter = Correct");
+        Debug.Log($"To play the game, you must: Push Up = higher, Push Down = lower, Enter = Correct");
+        Debug.Log($"Is your number higher or lower than {guess}?");
+        // Fixing the bug that max doesn't reach 999:
+        max++;
     }
 
     // Update is called once per frame
@@ -25,16 +27,16 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("The Number was Higher.");
             min = guess;
-            Debug.Log(min);
+            guess = ((max + min) / 2);
+            Debug.Log($"Is the number {guess}? Press up for higher and down for lower or enter for yes!");
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("The Number was Lower.");
             max = guess;
-            Debug.Log(max);
-    
+            guess = ((max + min) / 2);
+            Debug.Log($"Is the number {guess}? Press up for higher and down for lower or enter for yes!");
+
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
