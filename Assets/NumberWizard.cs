945c2a49bcc7ff49cd506e87d1162744175d1321
Alex <akharoukpp@gmail.com>
2018-12-23 15:16:23 +0000

    REFACTOR and encapsulate methods.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class NumberWizard : MonoBehaviour
 {
     // Setting our variables:
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max;
+    int min;
+    int guess;
     string title = "Magical Number Wizard";
 
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();
+    }
 
+    void StartGame()
+    {
+        // Resetting Our Variables:
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        // Encapsulating the code into a function, calling it after in our Start Method
         Debug.Log($"Welcome to the {title}!");
         Debug.Log($"Please pick a number between {min} and {max}");
         Debug.Log($"To play the game, you must: Push Up = higher, Push Down = lower, Enter = Correct");
         Debug.Log($"Is your number higher or lower than {guess}?");
+
         // Fixing the bug that max doesn't reach 999:
         max++;
+
     }
 
     // Update is called once per frame
@@ -27,26 +39,39 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            min = guess;
-            guess = ((max + min) / 2);
-            Debug.Log($"Is the number {guess}? Press up for higher and down for lower or enter for yes!");
+            NextGuess("min");
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            max = guess;
-            guess = ((max + min) / 2);
-            Debug.Log($"Is the number {guess}? Press up for higher and down for lower or enter for yes!");
-
+            NextGuess("max");
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             if (guess == 500)
             {
                 Debug.Log("Really? Your number is going to be 500?");
+                StartGame();
             } else
             {
                 Debug.Log("Woohoo! A.I level achieved.");
+                StartGame();
             }
+        }
+    }
+
+    void NextGuess(string isMin)
+    {
+        if (isMin == "min")
+        {
+            min = guess;
+            guess = ((max + min) / 2);
+            Debug.Log($"Is the number {guess}? Press up for higher and down for lower or enter for yes!");
+        }
+        else
+        {
+            max = guess;
+            guess = ((max + min) / 2);
+            Debug.Log($"Is the number {guess}? Press up for higher and down for lower or enter for yes!");
         }
     }
 }
